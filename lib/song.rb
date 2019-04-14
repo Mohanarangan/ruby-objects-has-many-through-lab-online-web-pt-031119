251bd2e4211732bc9c28f3f4565283fe82abd44a
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-14 23:08:32 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,19 @@
+class Song
+  
+  attr_accessor :name, :artist, :genre
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @genre = genre
+    @@all << self
+    
+  end
+  
+  def self.all
+    @@all
+  end
+  
+end
+  
