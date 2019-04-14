c7165554d8f64c0b9a9f763735f76d944077bf4b
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-14 22:56:41 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,12 +7,14 @@ class Artist
   def initialize(name)
     @name = name
     @@all << self
+    @song = song
   end
   
   def self.all
     @@all
   end
   
-  
+  def new_song(name, genre)
+  end
   
 end
