b0e9f45f257350fc00b5e625407793a323b95f59
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-15 02:52:30 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,7 +16,7 @@ class Patient
   end
   
   def appointments
-    Appointment.all.select do |appointments|
+    Appointment.all.select do |appointment|
       appointment.patient == self
     end
   end
