c24d5c6c803f32c3e0a01b5a2e23eac1ffbf5a34
chandra mohan <mohaniyer_78@hotmail.com>
2019-04-15 01:31:39 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,2 +1,16 @@
 def Appointment
+  @@all= []
+  
+  
+  def initialize(date,patient,doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@ll << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
